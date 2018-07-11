08099c6055608ce62756eff414fc6f25f2e510bd
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2018-07-12 00:43:12 +0300

    scope some styles

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -12,84 +12,86 @@
 </template>
 
 <script>
-
-  export default {
-    name: 'landing-page',
-    methods: {
-      login() {
-        this.loggingIn = true;
-        this.failedLogin = false;
-        this.$electron.ipcRenderer.send('login', this.sessionID)
-      },
-      handleLoginResponse(event ,data){
-        this.loggingIn = false
-        if(data === false){
-          this.failedLogin = true;
-        }else{
-          this.$router.push({path: 'main'})
-        }
-        console.log(data)
-      }
+export default {
+  name: "landing-page",
+  methods: {
+    login() {
+      this.loggingIn = true;
+      this.failedLogin = false;
+      this.$electron.ipcRenderer.send("login", this.sessionID);
     },
-    data() {
-      return {
-        sessionID: "",
-        failedLogin: false,
-        loggingIn: false,
+    handleLoginResponse(event, data) {
+      this.loggingIn = false;
+      if (data === false) {
+        this.failedLogin = true;
+      } else {
+        this.$router.push({ path: "main" });
       }
-    },
-    mounted(){
-      this.$electron.ipcRenderer.on('login', this.handleLoginResponse)
+      console.log(data);
     }
+  },
+  data() {
+    return {
+      sessionID: "",
+      failedLogin: false,
+      loggingIn: false
+    };
+  },
+  mounted() {
+    this.$electron.ipcRenderer.on("login", this.handleLoginResponse);
   }
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  .btn {
-    min-width:200px;
-    height: 30px;
-    border:none;
-    
-  }
+.heading {
+  margin-bottom: 30px;
+  text-align: center;
+}
 
-  .heading{
-    margin-bottom:30px;
-    text-align: center;
-  }
+.text-center {
+  text-align: center;
+}
 
-  .text-center{
-    text-align: center;
-  }
+#login-container {
+  position: absolute;
+}
 
-  #login-container{
-    position: absolute;
-  }
+.warn {
+  color: red;
+}
 
-  .warn{
-    color:red;
-  }
+#wrapper {
+  margin: auto;
+  margin-top: 20px;
+  width: 200px;
+}
 
-  #wrapper{  
-    margin:auto;
-    margin-top:20px;
-    width: 200px;
-  }
+body {
+  font-family: "Source Sans Pro", sans-serif;
+  background: #222;
+  color: white;
+}
+</style>
+<style scoped>
+.form-control {
+  min-width: 200px;
+  height: 30px;
+  border: 1px solid green;
+  margin-bottom: 5px;
+}
 
-  .form-control {
-    min-width: 200px;
-    height: 30px;
-    border: 1px solid green;
-    margin-bottom:5px;
-  }
-
-  body { font-family: 'Source Sans Pro', sans-serif; background: #222; color:white; }
-
+.btn {
+  min-width: 200px;
+  height: 30px;
+  border: none;
+}
 </style>
