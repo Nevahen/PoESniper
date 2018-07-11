cb093e2a84d79bfb3d4c961f04b2ddfb6fa060c1
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2018-07-11 22:11:13 +0300

    Don't  allow adding same search term multiple times

--- src/main/searchManager.js
+++ src/main/searchManager.js
@@ -6,6 +6,15 @@ export class SearchManager {
   }
 
   addSearchItem(searchItem) {
+    let count = this.searchItems.filter(elem => {
+      return elem.term === searchItem.term;
+    }).length;
+
+    if (count > 0) {
+      console.log("A search with this term is already registered.");
+      return;
+    }
+
     searchItem.manager = this;
     this.searchItems.push(searchItem);
   }
